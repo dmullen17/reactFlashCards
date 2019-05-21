005ab17ccda2205090eeab27f72b30ae7652123d
dmullen17 <dmullen17@gmail.com>
2019-05-21 16:32:10 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+const questionBank = require('./questions.json');
+
+class FlashCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            question: 'Welcome to my flashcards application!  Happy learning 🙃'
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        const index = Math.floor(Math.random() * questionBank.questionsList.length);
+        this.setState({
+            question: questionBank.questionsList[index].question
+        });
+    }
+    render() {
+        return (
+            <div className='flashCard' onClick={this.handleClick}>{this.state.question}
+            </div>
+        );
+    }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <FlashCard />
     </div>
   );
 }
