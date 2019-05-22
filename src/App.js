3df328e820e49cfaefc103a8fa21a712cd647aba
dmullen17 <dmullen17@gmail.com>
2019-05-22 12:58:31 -0700

    added bootstrap + topics row

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-const questionBank = require('./questions.json');
+let questionBank = require('./questions.json');
+questionBank.questionsList = questionBank.questionsList.filter(obj => (obj.question !== '' && obj.question !== 'PLACEHOLDER'));
 
 class FlashCard extends React.Component {
     constructor(props) {
@@ -24,10 +25,30 @@ class FlashCard extends React.Component {
     }
 }
 
+class Topics extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTopics: ['JavaScript', 'React', 'CSS']
+        };
+    }
+    render() {
+        const selectedTopics = this.state.selectedTopics.map(topic => <div className='btn btn-success'>{topic}</div>);
+        return(
+            <div>Topics:{selectedTopics}</div>
+        );
+    }
+}
+
 function App() {
   return (
     <div className='App'>
-      <FlashCard />
+      <div class='row' style={{margin:'0px'}}>
+        <Topics />
+      </div>
+      <div class='row' style={{margin:'0px'}}>
+        <FlashCard />
+      </div>
     </div>
   );
 }
