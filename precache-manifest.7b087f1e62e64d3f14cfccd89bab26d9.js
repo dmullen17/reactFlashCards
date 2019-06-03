ce17254df64dbdd9476d537e128337da5476bd5c
dmullen17 <dmullen17@gmail.com>
2019-06-03 11:14:15 -0700

    Updates

---
+++ precache-manifest.7b087f1e62e64d3f14cfccd89bab26d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc301d3410ff4bbd079959681f34be3",
+    "url": "/reactFlashCards/index.html"
+  },
+  {
+    "revision": "0a638df9ed4976d23b1c",
+    "url": "/reactFlashCards/static/css/2.d3366266.chunk.css"
+  },
+  {
+    "revision": "4cda004781224d2ac407",
+    "url": "/reactFlashCards/static/css/main.667b8de0.chunk.css"
+  },
+  {
+    "revision": "0a638df9ed4976d23b1c",
+    "url": "/reactFlashCards/static/js/2.b0867797.chunk.js"
+  },
+  {
+    "revision": "4cda004781224d2ac407",
+    "url": "/reactFlashCards/static/js/main.5fc7a502.chunk.js"
+  },
+  {
+    "revision": "6cc29ab661032784609f",
+    "url": "/reactFlashCards/static/js/runtime~main.e4b287f7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/reactFlashCards/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/reactFlashCards/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/reactFlashCards/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/reactFlashCards/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/reactFlashCards/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "bc847bd0b108ba56ede0266b9066ff43",
+    "url": "/reactFlashCards/static/media/joao-silas-65013-unsplash.bc847bd0.jpg"
+  }
+]);
